8d66ca15ab859bf376d7748ce69e094d6329b246
Michael Gyimadu <michaelgyimadu@gmail.com>
2025-03-23 15:30:36 -0400

    reduced small screen font size for hero text to 2xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <div className="mb-10">
             <div className="flex flex-col lg:flex-col lg:gap-10">
               <div className='flex flex-col justify-center lg:pt-10 mb-6'>
-                <div className='font-f1 text-3xl sm:text-3xl lg:text-4xl mb-7'>Hi,<br></br>I&apos;m Michael Gyimadu!</div>
+                <div className='font-f1 text-2xl sm:text-3xl lg:text-4xl mb-7'>Hi,<br></br>I&apos;m Michael Gyimadu!</div>
                 <div className='text-sm sm:text-lg lg:text-base'>I&apos;m a Machine Learning Researcher focused on predictive modelling for financial data.<br /><br />
                     Additionally, I&apos;m a sophomore at Wingate University, pursuing a degree in Mathematics and Economics.<br />                    
                 </div>
